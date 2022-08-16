4596722505bf0e390f75accbea3c6ed2a77ce785
Monroe Clinton <contact@monroeclinton.com>
2022-08-15 22:58:45 -0400

    More info about xinit in preview.sh

--- preview.sh
+++ preview.sh
@@ -3,7 +3,11 @@
 # Stop running if we hit an error
 set -e
 
-# Run xinit then specify Xephyr server with a display of 1 to use.
-# In X11 this basically means that an X server runs on localhost:1.0
-# If display 1 is in use, change it to another number like 10
+# The xinit command takes a first option of a xinitrc script to run
+# and a second option of the command to start an X server.
+
+# Run xinit then specify Xephyr command to start a nested X server
+# with a display of 1 to use. In X11 this basically means that
+# an X server runs on localhost:1 If display 1 is in use, change it
+# to another number like 10.
 xinit ./xinitrc -- $(command -v Xephyr) :1 -screen 1024x768
